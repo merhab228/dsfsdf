3f83c7b210fca1fc3d87959df408af977c26ff3c
merhab228 <ggggtvink228@gmail.com>
2024-06-05 09:17:08 +0300

    first commit:

---
+++ helper/config_data/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from environs import Env
+from typing import Union
+
+
+@dataclass
+class TgBot:
+    token: str  # Токен для доступа к телеграм-боту
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: Union [str, None] = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
